177ca36a0d44f3be54aa05f04ca873beb7e88aef
vantammega <vantammega@gmail.com>
2016-09-20 14:36:03 +0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: tamvv
+ *
+ * Created on September 19, 2016, 2:51 PM
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf ("Hello c");    
+    return 0;
+}
+
